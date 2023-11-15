41b13f8c816315d12d81391395dda3f3ded57793
Dvir Dor <dvirdor123121@gmail.com>
2023-11-15 20:42:12 +0200

    Add conditions for stopping the growth of the ball

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
 function onBallClick(){
     const elBall = document.querySelector('.ball')
   var currSize = parseInt(elBall.offsetWidth, 10)
+  if (currSize > 400) {
+    elBall.style.width = 100 + 'px'
+    elBall.style.height = 100 + 'px'
+    return
+  }
 
   elBall.style.width = currSize + 50 + 'px'
   elBall.style.height = currSize + 50 + 'px'
